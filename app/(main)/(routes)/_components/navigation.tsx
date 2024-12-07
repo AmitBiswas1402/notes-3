ff28dd675d2b48a4da9656256df7a08c8cc7da25
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-12-07 22:10:05 +0530

    no time

--- app/(main)/(routes)/_components/navigation.tsx
+++ app/(main)/(routes)/_components/navigation.tsx
@@ -18,7 +18,7 @@ export const Navigation = () => {
   return (
     <>
       <aside className={cn("group/sidebar h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]", isResetting && "transition-all ease-in-out duration-300", isMobile && "w-0")}>
-        <div className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+        <div className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition", isMobile && "opacity-100")}>
           <ChevronLeft className="h-6 w-6" />
         </div>
         <div>
@@ -29,6 +29,10 @@ export const Navigation = () => {
         </div>
         <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
+      <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all ease-in-out duration-300", isMobile && "left-0 w-full")}
+      >
+        
+      </div>
     </>
   );
 };
